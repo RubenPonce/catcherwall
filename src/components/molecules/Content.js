c6e8a3f30d95c9091423049a92e0cf6b146baf71
rubenponce <rubenmartinponce@gmail.com>
2023-10-05 11:29:40 -0400

    Update index.css: change color of .time-ago to var(--primary-color) !important, remove max-width property from .subtitle.
    Update Content.js: add title attribute to Card.Link, truncate Card.Title text, add ellipsis to the end of the title.

--- src/components/molecules/Content.js
+++ src/components/molecules/Content.js
@@ -2,6 +2,7 @@ import {filterWords} from "../../utils/filterwords";
 import {useContext, useMemo} from "react";
 import {TabContext} from "../atoms/Tab";
 import {Card, Container, Image} from "react-bootstrap";
+
 function timeAgo(minutes) {
     if (minutes < 60) {
         return `${minutes} minutes ago`;
@@ -27,20 +28,22 @@ export const Content = ({content, name, index}) => {
     const title = useMemo(() => {
         return isFilterEnabled ? filterWords(content.title) : content.title;
     }, [isFilterEnabled, content.title]);
-const contentUploadedMinutesAgo = new Date(content.date);
-const diffInMinutes = timeAgo(Math.floor((now - contentUploadedMinutesAgo) / 60000) );
+    const contentUploadedMinutesAgo = new Date(content.date);
+    const diffInMinutes = timeAgo(Math.floor((now - contentUploadedMinutesAgo) / 60000));
 
     return (
         <Card className="mb-4">
             <Card.Body className="content-piece">
                 <Container fluid>
 
-                    <Card.Link href={content.url} target="_blank" rel="noopener noreferrer">
+                    <Card.Link href={content.url} target="_blank" rel="noopener noreferrer" title={title}>
                         <Card.Text className="content-number">{index + 1}</Card.Text>
 
-                        <Card.Text className={!diffInMinutes.includes("day") ? "time-ago" : ""}>{diffInMinutes}</Card.Text>
-                        <Image src={content.image} alt={title} onError={handleError} loading="lazy" rounded className="content-image mb-2" />
-                    <Card.Title className="title truncate">{title}</Card.Title>
+                        <Card.Text
+                            className={!diffInMinutes.includes("day") ? "time-ago" : ""}>{diffInMinutes}</Card.Text>
+                        <Image src={content.image} alt={title} onError={handleError} loading="lazy" rounded
+                               className="content-image mb-2"/>
+                        <Card.Title className="title truncate">{title}...</Card.Title>
 
                         <Card.Text className="date">{content.date}</Card.Text>
                         <Card.Text className="content-name">{content.authorName}</Card.Text>
